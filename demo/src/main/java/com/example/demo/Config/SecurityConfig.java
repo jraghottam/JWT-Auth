dd3f2d56fb2e3b4cea7df50a35b1da3bd6a70b0b
jraghottam <51281053+jraghottam@users.noreply.github.com>
2023-12-07 19:07:04 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/demo/Config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.example.demo.Config;
+
+import com.example.demo.Security.JWTAuthenticationFilter;
+import com.example.demo.Security.JwtAuthenticationEntryPoint;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private JwtAuthenticationEntryPoint point;
+    @Autowired
+    private JWTAuthenticationFilter filter;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeRequests()
+                .requestMatchers("/user").permitAll().requestMatchers("/Auth/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(point))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider Provider = new DaoAuthenticationProvider();
+        Provider.setUserDetailsService(userDetailsService);
+        Provider.setPasswordEncoder(passwordEncoder);
+        return Provider;
+    }
+
+//    @Bean
+//    public PasswordEncoder getPasswordEncoder() {
+//        return passwordEncoder;
+//    }
+}
